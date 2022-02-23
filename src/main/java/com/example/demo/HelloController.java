222b333cb66f26762adacb6ab405b737af7993ed
Michael <mrvbifchik176@gmail.com>
2022-02-23 12:53:50 +0600

    AITUMAP

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
+
+import java.io.File;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.effect.ImageInput;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 import java.nio.Buffer;
 
@@ -15,6 +24,7 @@ public class HelloController {
     private ImageView map;
     //Image image = new ImageInput("C:\\Users\\Michael\\IdeaProjects\\demo\\images\\floor2.png");
     //Image Image = new Image("C:\\Users\\Michael\\IdeaProjects\\demo\\images\\floor2.png");
+   // File f = new File(C:\\Users\\Michael\\Downloads\\second.jpg);
 
 
 
